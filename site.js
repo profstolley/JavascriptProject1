d5eb412ca21cd60965f65815f3b1be0820eefaed
Karl Stolley <karl.stolley@gmail.com>
2017-09-18 17:59:34 -0500

    Clean up data; fix buildingname reference

--- site.js
+++ site.js
@@ -1,5 +1,5 @@
 var buildings = [{
-  "name": "Galvin Libray",
+  "name": "Galvin Library",
   "address": "35 W 33rd St, Chicago, IL",
   "location":"https://www.google.com/maps/place/Paul+V.+Galvin+Library/@41.833676,-87.628307,15z/data=!4m5!3m4!1s0x0:0xd02a6e76ebf8681b!8m2!3d41.833676!4d-87.628307"
 },
@@ -7,10 +7,7 @@ var buildings = [{
   "name": "S.R.Crown Hall",
   "address": "3360 S State St, Chicago, IL",
   "location":"https://www.google.com/maps/place/S.+R.+Crown+Hall/@41.8343335,-87.6275423,17z/data=!3m1!5s0x880e2c0d0aad0f2f:0x18bd61b0ced378cc!4m12!1m6!3m5!1s0x0:0x91cbe4661afb6f1a!2sIllinois+Institute+of+Technology!8m2!3d41.8348731!4d-87.6270059!3m4!1s0x0:0x70210951aac6ba0f!8m2!3d41.8331937!4d-87.627275"
-},
-get location() {
-			return this.name.address.location
-			}
+}
 ]
 
 /* $('#building-form').append('<img src="' + building['avatar_url'] +'" />');
@@ -19,7 +16,7 @@ building image?*/
 $('#building-form').on('submit', function(e) {
   var buildingname = $('#buildingname').val();
   $('#results').append('<li><a href="https://www.google.com/maps/place/' +
-  buildingname + '">' + budilingname + '</a></li>');
+  buildingname + '">' + buildingname + '</a></li>');
   e.preventDefault();
 });
 $('#buildingname').on('focus', function() {
